ef21cfe3cda5d6f4ab7ad65040fe0b7ddaeda605
chillingloccini <chillingloccini1@gmail.com>
2024-08-25 13:20:59 +0100

    Setting up Sequelize DB

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
+import sequelize from './config/database';
 
 
 const app:Application = express();
@@ -10,5 +11,11 @@ app.use(bodyParser.json());
 app.get('/', (req:Request, res:Response) => {
     res.send('Server is running I am here I am don solo cio!');
 });
+
+// Database connection
+sequelize.sync({ alter: true }).then(() => {
+    console.log('Database connected successfully');
+}).catch((err) => console.log('Error connecting to the database', err));
+
 
 export default app;
--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize';
 
 const sequelize = new Sequelize({
-    database: process.env.DB_NAME || 'my-db',
+    database: process.env.DB_DATABASE_NAME,
     dialect: process.env.DB_DATABASE_DIALECT as Dialect || 'mysql',
     username: process.env.DB_DATABASE_USER || 'root',
     password: process.env.DB_DATABASE_PASSWORD || '',
