5c453bb603305dffd5506cb4308fb962c80bbea0
chillingloccini <chillingloccini1@gmail.com>
2024-08-25 13:02:55 +0100

    Setting up Sequelize DB

---
+++ src/config/database.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Dialect } from 'sequelize';
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME || 'my-db',
+    dialect: process.env.DB_DATABASE_DIALECT as Dialect || 'mysql',
+    username: process.env.DB_DATABASE_USER || 'root',
+    password: process.env.DB_DATABASE_PASSWORD || '',
+    host: process.env.DB_DATABASE_HOST || 'localhost',
+    models: [__dirname + "/database/models"],
+});
+
+export default sequelize;
